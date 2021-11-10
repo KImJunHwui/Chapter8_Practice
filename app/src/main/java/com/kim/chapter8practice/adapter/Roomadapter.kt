bd0c7541a0380267dac0b12d2ec265a3f4a65b6a
KJH <daeding7@naver.com>
2021-11-10 20:15:30 +0900

    챕터 8강 - 2 완료

---
+++ app/src/main/java/com/kim/chapter8practice/adapter/Roomadapter.kt
@@ -0,0 +1,31 @@
+package com.kim.chapter8practice.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.kim.chapter8practice.R
+import com.kim.chapter8practice.datas.Room
+
+class Roomadapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList) {
+
+        val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = inflater.inflate(R.layout.activity_room_list_item, null)
+        }
+
+        val row = tempRow!!
+        return row
+    }
+
+
+
+}
